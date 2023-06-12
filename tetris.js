bf475bb85a4f6f52794d2bdb72568467e3a74aec
Beni Cherniavsky-Paskin <cben@redhat.com>
2023-06-12 10:35:23 +0300

    PoC: Edit-eval-render loop
    no self-modifying yet

---
+++ tetris.js
@@ -0,0 +1,50 @@
+var w = 10
+var h = 20
+
+// Sets of coordinates are a fun representation, but JS Set doesn't support tuple keys :-(
+var formatRC = JSON.stringify
+var parseRC = JSON.parse
+
+var RCSet = pairs => new Set(pairs.map(formatRC))
+var mapRC = (rcset, f) => RCSet([...rcset].map(parseRC).map(f))
+
+var shape = RCSet([[0,4], [0,5],
+                   [1,4],
+                   [2,4]])
+
+var shift = (board, deltaRow, deltaCol) =>
+  mapRC(board, ([r, c]) => [r + deltaRow, c + deltaCol])
+
+var Cell = ({ rc, color }) =>
+  html`<td style="border: grey solid 1px; background-color: ${color}; color: grey;">${rc}</td>`
+
+var Board = ({ set }) => 
+  html`<table>
+    ${range(0, h).map(r => 
+      html`<tr>
+        ${range(0, w).map(c => {
+          const rc = formatRC([r, c])
+          const color = set.has(rc) ? 'blue' : 'white'
+          return html`<${Cell} rc=${rc} color=${color}/>`
+        })}
+      </tr>`
+    )}
+  </table>`
+
+var state = shape
+state = shift(state, 1, 0)
+state = shift(state, 0, 1)
+state = shift(state, 1, 0)
+state = shift(state, 1, 0)
+state = shift(state, 1, 0)
+state = shift(state, 1, 0)
+state = shift(state, 1, 0)
+
+
+
+var App = (props) =>
+  html`<div>
+    <div>Score: 0</div>
+    <div><${Board} set=${state}/></div>
+  </div>`
+// window.App = App
